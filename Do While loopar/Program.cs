519c3a250ff313d68f146d1c0b99e26656979dd5
TheBlueSoul5188 <hibba04@hotmail.se>
2022-10-07 10:54:35 +0200

    Add project files.

---
+++ Do While loopar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgift4_2
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            int i = 50;
+            while (i>0) {
+                Console.WriteLine(i);
+                i--;
+            }
+        }
+    }
+}
